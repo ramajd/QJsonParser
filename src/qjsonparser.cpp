92b54256e866319d7b593add49123d2acf36750f
Reza Alizadeh Majd <r.a.majd@gmail.com>
2013-12-16 19:49:26 +0330

    QJsonParser constructor implemented.

--- src/qjsonparser.cpp
+++ src/qjsonparser.cpp
@@ -1,7 +1,33 @@
 #include "qjsonparser.h"
 
+#include <QtScript/QScriptValue>
+#include <QtScript/QScriptEngine>
+
+struct QJsonData {
+
+    QScriptEngine engine;
+    QScriptValue parseFn;
+    QScriptValue serializeFn;
+
+};
+
 QJsonParser & QJsonParser::instance()
 {
     static QJsonParser theInstance;
     return theInstance;
+}
+
+
+QJsonParser::QJsonParser()
+{
+    d = new QJsonData;
+
+    const QString script =
+            "function parse_json(string) { return JSON.parse(string); }\n"
+            "function serialize_json(object) { return JSON.stringify(object); }";
+
+    QScriptValue result = d->engine.evaluate(script);
+
+    d->parseFn = d->engine.globalObject().property("parse_json");
+    d->serializeFn = d->engine.globalObject().property("serialize_json");
 }
